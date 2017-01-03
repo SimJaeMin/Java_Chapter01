74ce5a6d41c65056d6b434eeba33297229725dc9
T3AD20 <T3AD20@G209-15>
2017-01-03 15:39:20 +0900

    final test

---
+++ src/chapter01/Switch.java
@@ -0,0 +1,104 @@
+package chapter01;
+
+public class Switch {
+public static void main(String[] args) {
+	
+	
+	int month=13;
+	int days=10;
+	switch(month)
+	{
+	case 1:
+	{
+	days=1;	
+	break;
+	}
+	case 2:	
+	{
+		days=2;
+		break;
+	}	
+	
+	case 3:	
+	{	
+		days=3;
+		break;
+	}	
+	
+	case 4:	
+	{	
+		days=4;
+		break;
+	}	
+	
+	case 5:	
+	{	
+		days=5;
+		break;
+	}	
+	
+	
+	case 6:	
+	{	
+		days=6;
+		break;
+	}	
+	
+	case 7:	
+	{	
+		days=7;
+		break;
+	}	
+	
+	case 8:	
+	{	
+		days=8;
+		break;
+	}	
+	
+	case 9:	
+	{	
+		days=9;
+		break;
+	}	
+	
+	case 10:	
+	{	
+		days=10;
+		break;
+	}	
+	
+	case 11:	
+	{	
+		days=11;
+		break;
+	}	
+	
+	case 12:	
+	{	
+		days=12;
+		break;
+	}	
+	
+	default:
+	{
+		System.out.println("해당월이 없습니다.");
+	
+	}	
+		
+		
+	}
+	
+	System.out.println(days);
+	
+}
+	
+
+
+
+
+
+
+
+
+}
